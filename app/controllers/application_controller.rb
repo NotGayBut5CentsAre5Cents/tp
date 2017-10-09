51fd27119d10833a23e6bfe83d5a1261fe7b2d2d
NotGayBut5CentsAre5Cents <telerik007@gmail.com>
2017-10-09 22:27:10 +0300

    It will work this time for sure

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,6 @@
 class ApplicationController < ActionController::Base
 	protect_from_forgery with: :null_session
 	def CSVRead(file)
-		text_file = TextFile.new({name: file.original_filename, content: file.read})
-		text_file.save
-		CSV.parse(text_file.content, converters: :numeric)              
+		CSV.parse(file.read, converters: :numeric)              
 	end
 end
